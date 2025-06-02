c1acbabac133964ec8c3627b57a76e8a8308fab7
devbuzzmint <dev@buzzmint.io>
2025-06-02 14:37:18 +0100

    Add test page to verify GitHub syncing

---
+++ app/test/page.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+export default function TestPage() {
+  return (
+    <div style={{ backgroundColor: 'red', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center', color: 'white', fontSize: '24px' }}>
+      This is a test page to verify GitHub syncing.
+    </div>
+  )
+} 
